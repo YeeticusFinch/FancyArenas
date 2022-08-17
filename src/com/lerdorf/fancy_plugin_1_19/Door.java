e9fe6d749d9b68d18946063033bbb1238f230c7d
Carl Lerdorf <50182007+YeeticusFinch@users.noreply.github.com>
2022-08-17 17:40:29 +0200

    Update Door.java

--- src/com/lerdorf/fancy_plugin_1_19/Door.java
+++ src/com/lerdorf/fancy_plugin_1_19/Door.java
@@ -8,6 +8,15 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitPlayer;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.Region;
 
 public class Door implements CommandExecutor, Serializable {
 	/**
@@ -17,6 +26,7 @@ public class Door implements CommandExecutor, Serializable {
 
 	public static transient HashMap<String, Door> doors = new HashMap<String, Door>();
 	
+	int x1, y1, z1, x2, y2, z2;
 	String name;
 	String[] open;
 	String[][] phases;
@@ -26,21 +36,46 @@ public class Door implements CommandExecutor, Serializable {
 	
 	public Door() {}
 	
-	public Door(Block[] blocks, boolean open) {
+	public Door(Block[] blocks, boolean open, int x1, int y1, int z1, int x2, int y2, int z2) {
 		set(blocks, open);
 		active = true;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.z1 = z1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.z2 = z2;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
         if(cmd.getName().equalsIgnoreCase("door")){
         	if (args.length == 3) {
         		Door door;
-        		if (doors.containsKey(args[0])) {
-        			door = doors.get(args[0]);
-        		} else {
-        			
-        		}
-        	}
+			ArrayList<Block> blocks = new ArrayList<Block>();
+			try {
+				Region r = Main.getWorldEdit().getWorldEdit().getSessionManager().get(new BukkitPlayer( Main.getWorldEdit(), player)).getSelection(new BukkitWorld(player.getWorld()));
+				sender.sendMessage("Successfully aquired WorldEdit selection");
+				int i = 0;
+				for (BlockVector3 e : r) {
+					Block block = (new Location(sender.getLocation().getWorld(), e.getX(), e.getY(), e.getZ())).getBlock();
+					if (block.getType() != Material.AIR && block.getType() != Material.CAVE_AIR && block.getType() != Material.VOID_AIR) {
+						blocks.add(block);
+					}
+					i++;
+				}
+				
+				BlockVector3 min = r.getMinimumPoint();
+				BlockVector3 max = r.getMaximumPoint();
+				
+				sender.sendMessage("Successfully created new door " + door.name));
+			} catch (IncompleteRegionException e) {
+				e.printStackTrace();
+				sender.sendMessage("Failed to get WorldEdit selection");
+			}
+        		
+        	} else {
+			sender.sendMessage("Error: Expected 3 args (name, phase, delay)");
+		}
         	return true;
         }
         return false;
