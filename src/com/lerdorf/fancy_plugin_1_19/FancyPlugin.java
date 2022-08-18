fa2bc7f1e4c1beb264f9e96b58d8cd176c4e6a8b
Carl Lerdorf <50182007+YeeticusFinch@users.noreply.github.com>
2022-08-18 16:03:14 +0200

    Update FancyPlugin.java

--- src/com/lerdorf/fancy_plugin_1_19/FancyPlugin.java
+++ src/com/lerdorf/fancy_plugin_1_19/FancyPlugin.java
@@ -14,6 +14,8 @@ import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 
 public class FancyPlugin extends JavaPlugin implements Listener {
 
+	public static ArrayList<Door> doors = new ArrayList<Door>();
+	
 	@Override
 	public void onEnable() {
 		System.out.println("Starting FancyPlugin");
@@ -21,15 +23,28 @@ public class FancyPlugin extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 		
 		// Register our command "kit" (set an instance of your command class as executor)
-        this.getCommand("wr").setExecutor(new WorldReset());
-        this.getCommand("worldreset").setExecutor(new WorldReset());
-        this.getCommand("sg").setExecutor(new SurvivalGames());
-        this.getCommand("survivalgames").setExecutor(new SurvivalGames());
-        this.getCommand("heal").setExecutor(new Heal());
-        this.getCommand("h").setExecutor(new Heal());
-        this.getCommand("healall").setExecutor(new Heal());
-        this.getCommand("ha").setExecutor(new Heal());
-        this.getCommand("door").setExecutor(new Door());
+		this.getCommand("wr").setExecutor(new WorldReset());
+		this.getCommand("worldreset").setExecutor(new WorldReset());
+		this.getCommand("sg").setExecutor(new SurvivalGames());
+		this.getCommand("survivalgames").setExecutor(new SurvivalGames());
+		this.getCommand("heal").setExecutor(new Heal());
+		this.getCommand("h").setExecutor(new Heal());
+		this.getCommand("healall").setExecutor(new Heal());
+		this.getCommand("ha").setExecutor(new Heal());
+		this.getCommand("door").setExecutor(new Door());
+		loadSaves();
+		
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+			  @Override
+			  public void run() {
+				  try {
+						doorUpdate();
+						//System.out.println("Main loop");
+			    	} catch (Exception e) {
+			    		e.printStackTrace();
+			    	}
+			  }
+			}, 0L, 1L);
 	}
 	
 	@Override
@@ -49,6 +64,43 @@ public class FancyPlugin extends JavaPlugin implements Listener {
 		p = Bukkit.getServer().getPluginManager().getPlugin("FastAsyncWorldEdit");
 		if (p instanceof WorldEditPlugin) return ((WorldEditPlugin) p);
 		return null;
+	}
+	
+	public void loadSaves() {
+		System.out.println("Attempting to load saved files for FancyPlugin");
+		File f = new File("plugins/.."); // How else do you get the current directory?
+		if (f != null) {
+			String[] list = f.list();
+			System.out.println("list = " + list);
+			if (list != null) {
+				for (String e : list) {
+					//System.out.println("File: " + e);
+					File g = new File(e + "/FancyPlugin/");
+					if (g.exists() && !e.equalsIgnoreCase("plugins")) {
+						System.out.println("Found " + e + "/FancyPlugin/");
+						String[] list2 = g.list();
+						for (String e2 : list2) {
+							System.out.println("File: " + e2 + " " + e2.substring(0, 4));
+							if (e.length() > 4 && e2.substring(0, 4).equals("door")) {
+								System.out.println("Loading " + e + "/FancyPlugin/" + e2);
+								int n = Integer.parseInt(e2.substring(4, e2.indexOf('.')));
+								while (doors.size() <= n) doors.add(null);
+								doors.set(n, new Door(e + "/FancyPlugin/" + e2));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public void doorUpdate() {
+		if (doors == null)
+			return;
+		for (Door d : doors) {
+			if (d != null)
+				d.update();
+		}
 	}
 	
 }
